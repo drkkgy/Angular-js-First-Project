e5b7413a33499d3cb6a92de6a0271380c966845c
ankit <drkkgy@gmail.com>
2018-06-26 13:14:04 +0530

    Completed my first assignement to create components

---
+++ basics-assignment-1-start/src/app/warning_Alert/warning_Alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning_Alert',
+  templateUrl: './warning_Alert.component.html',
+  styleUrls: ['./warning_Alert.component.css']
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
